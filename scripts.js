6d802dde4bae21b9117823827be7abb866455376
Serena Hasler <srhasler@yahoo.com>
2020-03-04 21:43:05 -0600

    Add while loops to ensure strings rather than numbers are entered

--- scripts.js
+++ scripts.js
@@ -4,10 +4,19 @@ book.lastName = prompt("What is your author's last name?");
 while (!book.lastName) {
   alert("This field cannot be left blank.");
   book.lastName = prompt("What is your author's last name?");
+}
+while (Number(book.lastName)) {
+  alert("You must enter a string, not a number.");
+  book.lastName = prompt("What is your author's last name?");
+}
 
 book.firstName = prompt("What is your author's first name?");
 while (!book.firstName) {
   alert("This field cannot be left blank.");
+  book.firstName = prompt("What is your author's first name?");
+}
+while (Number(book.firstName)) {
+  alert("You must enter a string, not a number.");
   book.firstName = prompt("What is your author's first name?");
 }
 
@@ -16,11 +25,23 @@ while (!book.title) {
   alert("This field cannot be left blank.");
   book.title = prompt("What is your title?");
 }
+while (Number(book.title)) {
+  alert("You must enter a string, not a number.");
+  book.title = prompt("What is your book's title?");
+}
 
-book.period = prompt("What is your book's period?");
+book.period = prompt("What is your book's literary period?");
 while (!book.period) {
   alert("This field cannot be left blank.");
   book.period = prompt("What is your book's literary period?");
 }
+while (Number(book.period)) {
+  alert("You must enter a string, not a number.");
+  book.period = prompt("What is the book's literary period?");
+}
 
 book.genre = prompt("What is your book's genre?");
+while (Number(book.genre)) {
+  alert("You must enter a string, not a number.");
+  book.genre = prompt("What is the book's genre?");
+}
