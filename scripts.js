6710baa950702c758b004f1116b298ed21c230b2
Serena Hasler <srhasler@yahoo.com>
2020-03-04 21:24:21 -0600

    Set up object and prompts to fill object

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const book = {};
+
+book.lastName = prompt("What is your author's last name?");
+book.firstName = prompt("What is your author's first name?");
+book.title = prompt("What is your book's title?");
+book.period = prompt("What is your book's period?");
+book.genre = prompt("What is your book's genre?");
